27e12cadbf4ddec506059b886278dead43be5cdf
umairnawaz333 <umair.nawaz1997@gmail.com>
2026-08-06 04:53:19 +0500

    Update lidflow.rb Cask formula with transparent RGBA icon and correct checksum

--- Casks/lidflow.rb
+++ Casks/lidflow.rb
@@ -1,6 +1,6 @@
 cask "lidflow" do
   version "1.0.0"
-  sha256 "f333ac2b4b2bd1d8209c1d1b82f44d074fe7b03c1540925b89cd24e689dbe6ff"
+  sha256 "7e13338158e4711a685f8b3cf454dd63a400949ef37278db68d42aacbcac50c6"
 
   url "https://github.com/umairnawaz333/LidFlow/releases/download/v#{version}/LidFlow.zip"
   name "LidFlow"
